1599043b09868a9e9b5377774e1f97bc3bc7919e
Shariful islam <sishajjad@shakti.org.bd>
2024-08-15 14:57:15 +0600

    navbar

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className={'flex justify-between items-center w-full mb-7'}>
+    <div className='flex justify-between items-center w-full mb-7'>
         {/*LEFT Side*/}
         <div className={'flex justify-between items-center gap-5'}>
             <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blur-100' onClick={()=>{}} >
@@ -21,6 +21,12 @@ const Navbar = () => {
         </div>
 
         {/*Right Side*/}
+
+        <div className="flex justify-between items-center gap-5">
+            <div className="hidden md:flex">
+          
+            </div>
+        </div>
     </div>
   );
 };
