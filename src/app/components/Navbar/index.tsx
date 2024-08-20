c33bc01f7e6d59f7d608413887a5e49384f1303a
Shariful islam <sishajjad@shakti.org.bd>
2024-08-20 16:04:11 +0600

    Sidebar

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Bell, Menu, Sun } from 'lucide-react'
+import { Bell, Link, Menu, Settings, Sun } from 'lucide-react'
 import React from 'react'
 
 
@@ -12,11 +12,12 @@ const Navbar = () => {
             <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blur-100' onClick={()=>{}} >
                 <Menu className='w-4 h-4'/>
             </button>
-        </div>
-        <div className="relative">
-          <input type='search' placeholder='Start type to search groups & products' className='pl-10 pr-4 w-50 md:w-80 border-2 border-gray-300 bg-white  rounded-lg focus:outline-none focus:border-blue-500' />
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
-              <Bell className='text-gray-500' size={20}/>
+        
+          <div className="relative">
+            <input type='search' placeholder='Start type to search groups & products' className='pl-10 pr-4 w-50 md:w-80 border-2 border-gray-300 bg-white  rounded-lg focus:outline-none focus:border-blue-500' />
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
+                <Bell className='text-gray-500' size={20}/>
+            </div>
           </div>
         </div>
 
@@ -36,7 +37,14 @@ const Navbar = () => {
                   </span>
                 </div>
                 <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3 " />
+                <div className="flex items-center gap-3 cursor-pointer">
+                    <div className="w-9 h-9"> image</div>
+                    <span className='font-semibold'>Shajjad</span>
+                </div>
             </div>
+            <Link href='/settings'>
+              <Settings className='cursor-pointer text-gray-500' size={24}/>
+            </Link>
         </div>
     </div>
   );
---
+++ src/app/components/Sidebar/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Menu } from 'lucide-react'
+import React from 'react'
+
+
+const Sidebar = ( )=> {
+  return (
+    <div>
+      {/* TOP LOGO */}
+        <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+          <div className="">Logo</div>
+          <h1 className='font-extrabold text-2xl'>Shajjad</h1>
+        
+          <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={()=>{}}>
+            <Menu className='w-4 h-4' />
+          </button>
+        </div>
+
+        {/* LINKS */}
+
+      <div className='flex-grow mt-8'>
+        {/* LINKS Here */}
+      </div>
+
+      {/* Footer Here */}
+
+      <div>
+          <p>&copy;</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
